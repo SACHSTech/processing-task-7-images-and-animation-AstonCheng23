cc55a691a02eb7babcd0c0b7a2c157ef3facd1fd
AstonCheng23 <yourGithubAccountEmail>
2022-04-13 14:07:20 -0400

    fooling around, temporary. made bouncing, images, and movement

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,24 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
+  int x = 512;
+  int y = 384;
+  
+  double xSpeed = 5;
+  double ySpeed = 5;
 	
+  
+  PImage img;
+  PImage background_room;
+  
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1024, 768);
   }
 
   /** 
@@ -16,20 +26,33 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 0, 0);
+    
+    img = loadImage("evil_eric_fabroa.jpg");
+    background_room = loadImage("Hyperspace-star-streaks-effect.jpg");  
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(background_room);    
+    image(img, x -50, y - 25);   
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    if ((x > width) || (x < 0)) {
+      xSpeed = xSpeed * -1.1;
+    }
+    if ((y > height) || (y < 0)) {
+      ySpeed = ySpeed * -1.1;
+    }
+  
+
+    x = (int) (x + (int) xSpeed);
+    y = (int) (y + (int) ySpeed);
+
+    println(x + " " + y);
+
+
   }
   
   // define other methods down here.
