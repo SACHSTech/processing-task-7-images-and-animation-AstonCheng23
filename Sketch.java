cee744ff7d9e9c5599bd90300669e2ecf6ce2de1
AstonCheng23 <yourGithubAccountEmail>
2022-04-17 16:14:18 -0400

    made everything

--- Sketch.java
+++ Sketch.java
@@ -2,23 +2,33 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
+	int width = 1024;
+  int height = 640;
+
   int x = 512;
-  int y = 384;
+  int y = 320;
   
-  double xSpeed = 5;
-  double ySpeed = 5;
-	
+  double xSpeed = 0;
+  double ySpeed = 0;
+
+  double xStep = 5;
+
+  int xProjectile = 512;
+  int yProjectile = 320;
+
+  double xProjectileSpeed = 5;
+  double yProjectileSpeed = 5;
   
   PImage img;
-  PImage background_room;
+  PImage backgroundRoom;
+  PImage starProjectile;
   
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(1024, 768);
+    size(width, height);
   }
 
   /** 
@@ -29,31 +39,71 @@ public class Sketch extends PApplet {
     background(0, 0, 0);
     
     img = loadImage("evil_eric_fabroa.jpg");
-    background_room = loadImage("Hyperspace-star-streaks-effect.jpg");  
+    backgroundRoom = loadImage("space.jpg");  
+    starProjectile = loadImage("april fools.jpg");
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    background(background_room);    
+    background(backgroundRoom);    
     image(img, x -50, y - 25);   
+    
+    if (frameCount == 120){
+      frameCount = 0;
+    }
 
     if ((x > width) || (x < 0)) {
       xSpeed = xSpeed * -1.1;
+      x = 0;
     }
     if ((y > height) || (y < 0)) {
       ySpeed = ySpeed * -1.1;
     }
-  
 
     x = (int) (x + (int) xSpeed);
     y = (int) (y + (int) ySpeed);
 
-    println(x + " " + y);
+    println(x + " " + y + " " + frameCount + " " + xProjectile + " " + yProjectile);
+
+    y = (int) (-0.002 * (x - 0)*(x - width));
+    x += 5;
+     
+    projectileEight(x, y);
 
 
   }
   
   // define other methods down here.
+
+  /**
+   * Makes the second projectile. xProjectileLast and yProjectileLast remember the last x and y positions of the fabroa, and changes every 60 seconds.
+   * @param xProjectileLast
+   * @param yProjectileLast
+   */
+  public void projectileEight(int xProjectileLast, int yProjectileLast){ 
+
+    if (frameCount == 60){
+      xProjectile = xProjectileLast;
+      yProjectile = yProjectileLast;   
+    }
+
+
+    image(starProjectile, xProjectile - 20, yProjectile - 19);
+
+
+    if ((xProjectile > width) || (xProjectile < 0)) {
+      xProjectileSpeed = (int) (xProjectileSpeed * -1.1);
+    }
+    if ((yProjectile > height) || (yProjectile < 0)) {
+      yProjectileSpeed = (int) (yProjectileSpeed * -1.1);
+    }
+
+    xProjectile = (int) (xProjectile + (int) xProjectileSpeed);
+    yProjectile = (int) (yProjectile + (int) yProjectileSpeed);
+
+      
+  }
+
 }
